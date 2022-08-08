62520bee336161f84daac6b4cda6cfa259c1aa47
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-08-08 12:27:53 +0200

    fix swagger

--- src/main/java/it/gov/pagopa/afm/calculator/controller/CalculatorController.java
+++ src/main/java/it/gov/pagopa/afm/calculator/controller/CalculatorController.java
@@ -1,20 +1,37 @@
 package it.gov.pagopa.afm.calculator.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import it.gov.pagopa.afm.calculator.model.PaymentOption;
+import it.gov.pagopa.afm.calculator.model.ProblemJson;
 import it.gov.pagopa.afm.calculator.model.calculator.CalculatorResponse;
+import it.gov.pagopa.afm.calculator.model.configuration.Configuration;
 import it.gov.pagopa.afm.calculator.service.CalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
+@Tag(name = "Calculator", description = "Everything about Calculator business logic")
 public class CalculatorController {
     @Autowired
     CalculatorService calculatorService;
 
 
-    @PostMapping("/calculate")
+    @Operation(summary = "Execute calculator logic", tags = {"Calculator"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = PaymentOption.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "429", description = "Too many requests", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Service unavailable", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class)))})
+    @PostMapping(value = "/calculate", produces = {MediaType.APPLICATION_JSON_VALUE})
     public CalculatorResponse calculate(@RequestBody PaymentOption paymentOption){
         return calculatorService.calculate(paymentOption);
     }
