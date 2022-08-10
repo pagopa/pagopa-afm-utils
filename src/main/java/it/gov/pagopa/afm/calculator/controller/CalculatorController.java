2b6313977a0f56738b38616ff284c53409ebcfd7
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-08-10 17:16:18 +0200

    added controller getFeesByPsp and getFees

--- src/main/java/it/gov/pagopa/afm/calculator/controller/CalculatorController.java
+++ src/main/java/it/gov/pagopa/afm/calculator/controller/CalculatorController.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.afm.calculator.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -12,6 +13,7 @@ import it.gov.pagopa.afm.calculator.model.calculator.Transfer;
 import it.gov.pagopa.afm.calculator.service.CalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,8 +24,37 @@ import java.util.List;
 @RestController()
 @Tag(name = "Calculator", description = "Everything about Calculator business logic")
 public class CalculatorController {
+
     @Autowired
     CalculatorService calculatorService;
+
+    @Operation(summary = "Get taxpayer fees of a specific idPSP", tags = {"Calculator"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = List.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "429", description = "Too many requests", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Service unavailable", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class)))})
+    @PostMapping(value = "/psps/{idPsp}/fees", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<Transfer> getFeeByPsp(
+            @Parameter(description = "PSP identifier", required = true) @PathVariable("idPsp") String idPsp,
+            @RequestBody PaymentOption paymentOption, @RequestParam(required = false, defaultValue = "10") Integer limit) {
+        paymentOption.setIdPspList(List.of(idPsp));
+        return calculatorService.calculate(paymentOption, limit);
+    }
+
+    @Operation(summary = "Get taxpayer fees of all or specified idPSP", tags = {"Calculator"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = List.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "429", description = "Too many requests", content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "500", description = "Service unavailable", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class)))})
+    @PostMapping(value = "/fees", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<Transfer> getFees(
+            @RequestBody PaymentOption paymentOption, @RequestParam(required = false, defaultValue = "10") Integer limit) {
+        return calculatorService.calculate(paymentOption, limit);
+    }
 
 
     @Operation(summary = "Execute calculator logic", tags = {"Calculator"})
