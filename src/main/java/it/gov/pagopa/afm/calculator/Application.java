7a86a2a3bc79cfe923e7efe1b10f21991930e469
Francesco Cesareo <cesareo.francesco@gmail.com>
2022-08-30 12:16:38 +0200

    configuration and code cleaning

--- src/main/java/it/gov/pagopa/afm/calculator/Application.java
+++ src/main/java/it/gov/pagopa/afm/calculator/Application.java
@@ -1,15 +1,29 @@
 package it.gov.pagopa.afm.calculator;
 
+import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 @EnableCaching
 public class Application {
 
+    @Value("${db.port}")
+    private String dbPort;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server inMemoryH2DatabaseServer() throws SQLException {
+        return Server.createTcpServer(
+                "-tcp", "-tcpAllowOthers", "-tcpPort", dbPort);
     }
 
 }
