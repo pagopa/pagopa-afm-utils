e3ba98d58ed348b9a87ce9cd25154e29a27ba792
Jacopo <jacopo.carlini@gmail.com>
2023-02-02 14:25:13 +0100

    fix

--- src/main/java/it/gov/pagopa/afm/utils/Application.java
+++ src/main/java/it/gov/pagopa/afm/utils/Application.java
@@ -12,8 +12,7 @@ import org.springframework.context.annotation.DependsOn;
 @DependsOn("expressionResolver")
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
