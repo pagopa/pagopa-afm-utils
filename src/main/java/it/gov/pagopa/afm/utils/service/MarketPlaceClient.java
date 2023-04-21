c19d12f0de3f54e6b1ceab42d74c1c8d17123b64
aacitelli <aacitelli@PDD-NB-0205.dgsgroup.it>
2023-04-21 17:01:29 +0200

    [PAGOPA-825] remove bundles by cdi: correction and Junit

--- src/main/java/it/gov/pagopa/afm/utils/service/MarketPlaceClient.java
+++ src/main/java/it/gov/pagopa/afm/utils/service/MarketPlaceClient.java
@@ -1,18 +1,25 @@
 package it.gov.pagopa.afm.utils.service;
 
-import feign.FeignException;
-import it.gov.pagopa.afm.utils.config.FeignConfig;
-import it.gov.pagopa.afm.utils.model.bundle.BundleRequest;
-import it.gov.pagopa.afm.utils.model.bundle.BundleResponse;
 import java.util.List;
+
+import javax.validation.constraints.Size;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import feign.FeignException;
+import io.swagger.v3.oas.annotations.Parameter;
+import it.gov.pagopa.afm.utils.config.FeignConfig;
+import it.gov.pagopa.afm.utils.model.bundle.BundleRequest;
+import it.gov.pagopa.afm.utils.model.bundle.BundleResponse;
 
 @FeignClient(
     value = "marketplace",
@@ -34,4 +41,16 @@ public interface MarketPlaceClient {
   @PostMapping(value = "/psps/{idPSP}/bundles/massive", consumes = MediaType.APPLICATION_JSON_VALUE)
   List<BundleResponse> createBundleByList(
       @PathVariable("idPSP") String idPSP, @RequestBody List<BundleRequest> body);
+  
+  @Retryable(
+      exclude = FeignException.FeignClientException.class,
+      maxAttemptsExpression = "${retry.marketplace.maxAttempts}",
+      backoff = @Backoff(delayExpression = "${retry.marketplace.maxDelay}"))
+  @DeleteMapping(value = "/psps/{idPSP}/bundles/{idBundle}", consumes = MediaType.APPLICATION_JSON_VALUE)
+  ResponseEntity<Void> removeBundle(
+      @Size(max = 35) 
+      @Parameter(description = "PSP identifier", required = true) 
+      @PathVariable("idPSP") String idPsp,
+      @Parameter(description = "Bundle identifier", required = true) 
+      @PathVariable("idBundle") String idBundle);
 }
--- src/test/java/it/gov/pagopa/afm/utils/controller/SyncControllerTest.java
+++ src/test/java/it/gov/pagopa/afm/utils/controller/SyncControllerTest.java
@@ -1,23 +1,21 @@
 package it.gov.pagopa.afm.utils.controller;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import it.gov.pagopa.afm.utils.service.CDIService;
-import java.util.ArrayList;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+
+import it.gov.pagopa.afm.utils.service.CDIService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -25,13 +23,7 @@ class SyncControllerTest {
 
   @Autowired private MockMvc mvc;
 
-  @Mock private CDIService cdiService;
-
-  @BeforeEach
-  void setUp() {
-    // precondition
-    when(cdiService.syncCDI()).thenReturn(new ArrayList<>());
-  }
+  @MockBean private CDIService cdiService;
 
   @Test
   void syncCDI() throws Exception {
@@ -47,6 +39,16 @@ class SyncControllerTest {
   @Test
   void syncCDIDeletion() throws Exception {
     String url = "/cdis/sync";
+    MvcResult result =
+        mvc.perform(delete(url).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+    assertNotNull(result.getResponse().getContentAsString());
+  }
+  
+  @Test
+  void syncDeleteBundlesByIdCDI() throws Exception {
+    String url = "/cdi/12345?pspCode=678901";
     MvcResult result =
         mvc.perform(delete(url).contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
