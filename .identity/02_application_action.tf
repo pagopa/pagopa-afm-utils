5588d9caaa62cd9bde7e70360629f2329382428f
Jacopo <jacopo.carlini@gmail.com>
2023-06-08 17:43:15 +0200

    fix

--- .identity/02_application_action.tf
+++ .identity/02_application_action.tf
@@ -70,15 +70,3 @@ resource "azurerm_key_vault_access_policy" "ad_kv_group_policy" {
   storage_permissions     = []
   certificate_permissions = []
 }
-
-resource "azurerm_key_vault_access_policy" "ad_domain_kv_group_policy" {
-  key_vault_id = data.azurerm_key_vault.domain_key_vault.id
-
-  tenant_id = data.azurerm_client_config.current.object_id
-  object_id = module.github_runner_app.object_id
-
-  key_permissions         = []
-  secret_permissions      = ["Get", "List"]
-  storage_permissions     = []
-  certificate_permissions = []
-}
